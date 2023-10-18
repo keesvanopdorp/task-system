89b2a34be017afc3f3e354404dc029f03973aedd
Kees van opdorp <35495762+keesvanopdorp@users.noreply.github.com>
2023-10-18 10:43:39 +0200

    feat(DB): changed schema

--- prisma/migrations/20231017123228_init/migration.sql
+++ prisma/migrations/20231017123228_init/migration.sql
@@ -35,5 +35,33 @@ CREATE TABLE `task` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `label` (
+    `name` VARCHAR(191) NOT NULL,
+    `color` VARCHAR(191) NOT NULL,
+    `board_id` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `label_name_key`(`name`),
+    PRIMARY KEY (`name`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_LabelToTask` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_LabelToTask_AB_unique`(`A`, `B`),
+    INDEX `_LabelToTask_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `task` ADD CONSTRAINT `task_board_id_fkey` FOREIGN KEY (`board_id`) REFERENCES `board`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `label` ADD CONSTRAINT `label_board_id_fkey` FOREIGN KEY (`board_id`) REFERENCES `board`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_LabelToTask` ADD CONSTRAINT `_LabelToTask_A_fkey` FOREIGN KEY (`A`) REFERENCES `label`(`name`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_LabelToTask` ADD CONSTRAINT `_LabelToTask_B_fkey` FOREIGN KEY (`B`) REFERENCES `task`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
